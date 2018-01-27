269ccce8ff65449e9246ce221c6538b1cc4b7c44
julie-oh <ohyeonjulie@gmail.com>
2018-01-27 14:22:42 +0900

    variable naming Algorithm to python

---
+++ python/camelCase.py
@@ -0,0 +1,27 @@
+def getCamelCase(inputValue):
+
+    # if first, end index is '_', let delete
+    inputValue = inputValue.strip("_")
+
+    # push arrList base on splited '_'
+    arr = inputValue.split("_")
+    reVal = ""
+
+    for i, idx in enumerate(arr):
+        arr[i] = arr[i].lower()
+        if i == 0:
+            reVal += str(arr[i])
+        else:
+            reVal += str(arr[i].capitalize())
+
+    print(reVal)
+
+
+
+if __name__ == "__main__":
+    getCamelCase("_test_case_yeon_joo")
+    getCamelCase("_test_case_yeon_joo_")
+    getCamelCase("teEt_case__________yeon_joo")
+    getCamelCase("TEST_case_yeon_jOo")
+    getCamelCase("________TEST_case_yeon_jOo")
+    getCamelCase("________TEST_case_yeon_jOo_______________")
